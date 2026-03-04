882fbdf0ad1f7b882ca7c1c92906066c29dc6318
ialikh72 <ialikh.w1@gmail.com>
2026-03-04 20:45:35 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] arr1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        Console.WriteLine("matrix:");
+        for (int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr1.GetLength(1); j++)
+                Console.Write(arr1[i, j] + " ");
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("transpose:");
+        for (int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr1.GetLength(1); j++)
+                Console.Write(arr1[j, i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
